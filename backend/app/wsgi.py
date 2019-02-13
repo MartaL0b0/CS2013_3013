6d72b400792228eacec095c4f4043fb911da1210
Jack O'Sullivan <jackos1998@gmail.com>
2019-02-13 17:33:34 +0000

    Backend: Add example db functionality

--- backend/app/wsgi.py
+++
@@ -1,4 +0,0 @@
-from briefthreat import app
-
-if __name__ == "__main__":
-    app.run()
--- backend/build/app.sh
+++ backend/build/app.sh
@@ -3,5 +3,5 @@
 if [ "$FLASK_ENV" == "development" ]; then
         exec python /opt/app/briefthreat.py
 else
-        exec gunicorn --workers $GUNICORN_WORKERS --bind :8080 --chdir /opt/app --user nobody --group nogroup wsgi:app
+        exec gunicorn --workers $GUNICORN_WORKERS --bind :8080 --chdir /opt/app --user nobody --group nogroup briefthreat:app
 fi
