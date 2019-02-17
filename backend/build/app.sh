b55042176cd9aced6c6f08dfa8aa13f0f76af29f
Jack O'Sullivan <jackos1998@gmail.com>
2019-02-17 20:30:47 +0000

    Backend: Fix Docker Python output buffering

--- backend/build/app.sh
+++ backend/build/app.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # main application entrypoint
 
+# python output buffering breaks stdout in docker
+export PYTHONUNBUFFERED=1
+
 if [ "$FLASK_ENV" == "development" ]; then
 	# use flask debug server in development
 	exec python /opt/app/briefthreat.py
