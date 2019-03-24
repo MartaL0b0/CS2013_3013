ebc11d506d1cecf2f0a2f440681a173ca6a5b100
Jack O'Sullivan <jackos1998@gmail.com>
2019-03-24 20:05:23 +0000

    Backend: Change test runner to keep containers on test completion

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -2,4 +2,5 @@
 COMMON_ARGS="-f docker-compose-test.yaml -p backend_test"
 
 docker-compose $COMMON_ARGS run --rm tests "$@"
-docker-compose $COMMON_ARGS down
+docker-compose $COMMON_ARGS exec briefthreat rm /tmp/test.db
+docker-compose $COMMON_ARGS stop
