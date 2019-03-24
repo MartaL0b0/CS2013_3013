1c11f0714d3e23d2c26bc19ae6655dc7b62b390c
Jack O'Sullivan <jackos1998@gmail.com>
2019-03-24 22:18:51 +0000

    Backend: Add tests for form retrieval and editing

--- backend/tests/util.py
+++ backend/tests/util.py
@@ -2,15 +2,15 @@ import urllib.parse
 
 from lxml import etree
 
-def extract_email_html_link(response):
+def extract_email_html_link(response, suffix=''):
     data = response.json()
     parser = etree.HTMLParser()
     tree = etree.fromstring(data['html'], parser=parser)
-    a_node = tree.findall('.//a')[0]
-    return {'email_link': a_node.attrib['href']}
+    a_node = next(filter(lambda a: a.attrib['href'].find('http') != -1, tree.findall('.//a')))
+    return {f'email_link{suffix}': a_node.attrib['href']}
 
-def extract_email_html_link_token(response):
+def extract_email_html_link_token(response, suffix=''):
     url = extract_email_html_link(response)['email_link']
     parsed = urllib.parse.urlparse(url)
     qs = urllib.parse.parse_qs(parsed.query)
-    return {'email_link': url, 'email_token': qs['token'][0]}
+    return {f'email_link{suffix}': url, f'email_token{suffix}': qs['token'][0]}
